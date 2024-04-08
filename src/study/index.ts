828f69d91d248f1029fb95d384a2bea3a51bd8b9
resforx <139689090+resforx@users.noreply.github.com>
2024-04-08 17:59:54 +0900

    commit rapidly for some reason
    testing code some much & async-await test

--- src/study/index.ts
+++ src/study/index.ts
@@ -1,5 +1,17 @@
-/* case 1 (initial) */
+import path from 'path';
+
 const testResults: { description: string; passed: boolean }[] = [];
+
+function relativeap() {
+  return path.relative(process.cwd(), __filename);
+}
+
+async function tasync<U>(func: () => Promise<U>, expected: string) {
+  const res = await func();
+  if (typeof res !== expected) {
+    throw new Error(`Expected ${expected} but received ${typeof res}`);
+  }
+}
 
 function expect(actual: any, expectedType: string) {
   const passed = typeof actual === expectedType;
@@ -21,16 +33,24 @@ function it(description: string, testCase: () => void) {
   try {
     console.log(`  ${description}`);
     testCase();
-    console.log('    ✔︎ Passed');
+    console.log(`    ✔︎ Passed ./${relativeap()}`);
   } catch (error) {
     console.error(`    ✘ Failed: ${(error as Error).message}`);
   }
 }
 
-function t(value: any) {
+interface InstanceReturn<K> {
+  expect: (expectedType: string) => void;
+  tasync: (func: Promise<K>, expected: string) => void
+}
+
+function t(value: any): InstanceReturn<{}> {
   return {
-    expect(expectedType: string) {
+    expect(expectedType) {
       expect(value, expectedType);
+    },
+    tasync(c, a) {
+      tasync(c, a);
     },
   };
 }
--- src/study/test.ts
+++ src/study/test.ts
@@ -20,3 +20,57 @@ title('Math operations', () => {
     t('Hello, ' + 'world!').expect('string');
   });
 });
+
+title("array and object", () => {
+  it("should be array", () => {
+    t([]).expect("any[]" || "object");
+  });
+
+  it("should be array_2", () => {
+    // t([1, 2, 3]).expect("number[]" || "");
+  });
+
+  it("should be array_3", () => {
+    t(["hello", "world"]).expect("string[]" || "object");
+  });
+
+  it("should be Object", () => {
+    t({}).expect("object");
+  });
+});
+
+const a = {};
+console.log(typeof a);
+const b = { a: 1 };
+console.log(typeof b);
+
+const t2 = [1, 2, 3];
+console.log(typeof t2);
+
+function returng(id: number | string) {
+  return { id, name: `user${id}@fake.com`}
+}
+
+title("toequal jest test", () => {
+  it("toequal test", () => {
+    t(returng(1)).expect("object");
+  });
+});
+
+console.log(typeof returng);
+
+// title("spfunc test", () => {
+//   it("spfunc test", () => {
+//   });
+// });
+
+async function get() {
+  return { id: 2, name: "jonh", actual: "foo" }
+}
+
+title("async test", () => {
+  it("async test", async () => {
+    const res = await get();
+    t(res).tasync(get(), "object")
+  });
+});
